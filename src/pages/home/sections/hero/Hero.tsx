25fc21a818baaed732c55beef2a73b67961d6bb4
Polina <fokichevap@gmail.com>
2024-09-19 13:37:02 +0100

    "get in touch" button animation

--- src/pages/home/sections/hero/Hero.tsx
+++ src/pages/home/sections/hero/Hero.tsx
@@ -1,9 +1,9 @@
 import './Hero.scss';
-import { useRef } from 'react';
+import { createRef, useRef } from 'react';
 import Lottie from 'lottie-react';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { useViewportContext } from '../../../../contexts';
+import { useCursorContext, useViewportContext } from '../../../../contexts';
 
 
 import {
@@ -25,9 +25,6 @@ const Hero = ({ id, refProp }: { id: string, refProp: React.RefObject<HTMLDivEle
                 <Timer />
                 <ContactButton/>
                 { viewport.desktop ? <DesktopTextSection/> : <MobileTextSection/> }
-                {/* <SkullModel /> */}
-                {/* <Lottie animationData={globeLottie} loop={true} /> */}
-                {/* <Lottie animationData={eyeLottie} loop={true} /> */}
                 
             </div>
         </div>
@@ -35,10 +32,81 @@ const Hero = ({ id, refProp }: { id: string, refProp: React.RefObject<HTMLDivEle
 }
 
 const ContactButton = () => {
-    // TODO add hover animation logic (either fill in from left, or text flips up)
+    // https://gsap.com/community/forums/topic/14114-animating-fill-color-to-match-background-color-as-you-scroll-down-the-page/
+    // TODO for color fill when time
+    const containerRef = useRef<HTMLDivElement>(null);
+    const textArray = 'Get in Touch'.split('');
+    const textRefs = useRef(textArray.map(() => createRef<HTMLDivElement>()));
+    const tl = useRef<gsap.core.Timeline>();
+
+    const { onHoverClickable } = useCursorContext();
+    
+    const { contextSafe } = useGSAP(() => {
+        tl.current = gsap.timeline({ paused: true });
+        const params = {
+            duration: 0.2,
+            ease: 'power1.inOut'
+        };
+        const delay = 0.02;
+        
+        tl.current!.to(containerRef.current, {
+            borderColor: '#D6FF00',
+            duration: 1
+        }, 0);
+        textArray.forEach((_, index) => {
+            tl.current!
+                .to(textRefs.current[index].current, {
+                    y: '-150%',
+                    ...params
+                }, (index) * delay)
+                .set(textRefs.current[index].current, {
+                    y: '150%'
+                },
+                (index) * delay + params.duration)
+        })
+        textArray.forEach((_, index) => {
+            tl.current!
+                .to(textRefs.current[index].current, {
+                    y: '0%',
+                    ...params
+                }, (index) * delay + params.duration)
+        })
+    }, { scope: containerRef })
+
+    const onHover = contextSafe((hover: boolean) => {
+        onHoverClickable(hover); // cursor
+        // first time
+        if (tl.current?.paused()) { tl.current.play(); }
+        else if (tl.current?.isActive()) {
+            // if entering while active && forward, do nothing
+            // if entering while active && reversed, restart
+            if (hover && tl.current?.reversed()) {
+                tl.current?.restart();
+            }
+            // if leaving while active, reverse quickly
+            if (!hover) {
+                tl.current?.reverse()
+            }
+        }
+        // if entering/leaving while passive, play/reverse based on hover
+        else {
+            if (hover) {
+                tl.current?.restart();
+            }
+            else { tl.current?.reverse() }
+        }
+    });
+
     return (
-        <div className='get-in-touch'>
-            Get in Touch
+        <div
+            className='get-in-touch'
+            ref={containerRef}
+            onMouseEnter={() => onHover(true)}
+            onMouseLeave={() => onHover(false)}
+        >
+            { textArray.map((char, index) => (
+                <div ref={textRefs.current[index]}>{char === ' ' ? '\u00A0' : char}</div>
+            ))}
         </div>
     )
 }
