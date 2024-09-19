61167c4bc8b55189bd9504daf39bedce3a994553
Polina <fokichevap@gmail.com>
2024-09-19 12:12:53 +0100

    skull follows mouse movement

--- src/components/threejs/skullmodel/SkullModel.tsx
+++ src/components/threejs/skullmodel/SkullModel.tsx
@@ -1,25 +1,24 @@
 import './SkullModel.scss';
 
-import { AmbientLight, Mesh } from 'three';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { SpotLight, useGLTF, PerspectiveCamera, OrbitControls, useTexture } from '@react-three/drei';
-import { GLTFLoader, OBJLoader } from 'three/examples/jsm/Addons.js';
+import { Group, Mesh, Object3DEventMap } from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useGLTF, PerspectiveCamera, OrbitControls} from '@react-three/drei';
+import { useRef } from 'react';
+import { useMousePositionContext, useViewportContext } from '../../../contexts';
 
 const SkullModel = () => {
     return (
         <div className='skull-model'>
             <Canvas>
-                {/* <SpotLight /> */}
-                {/* <camera /> */}
                 <PerspectiveCamera makeDefault fov={30} position={[0,1,10]}/>
                 <OrbitControls
-                        // ref={orbitControlsRef}
-                        // enableZoom={false}
-                        // enablePan={false}
+                        enableRotate={false}
+                        enableZoom={false}
+                        enablePan={false}
                     />
                 {/* <ambientLight intensity={1}/> */}
                 <pointLight position={[0,0,10]} intensity={80}/>
-                <Model />
+                <Model/>
             </Canvas>
         </div>
     )
@@ -29,11 +28,32 @@ const Model = () => {
     const { nodes } = useGLTF('models/skull/human-skull.glb');
     const { human_skull_1: skullBone, human_skull_2: jawBone } = nodes;
     const meshes = [skullBone, jawBone] as Mesh[];
-
     const color = '#FFFFFF'; //BDB6B0
 
+    // mouse movement logic
+    const groupRef = useRef<Group<Object3DEventMap>>(null);
+    const { x, y } = useMousePositionContext();
+    const { measurements } = useViewportContext();
+    const { width, height } = measurements;
+    const pi = Math.PI;
+    const factor = 0.7;
+
+    const getRotation = () => {
+        const percentageX = x/width - 0.5;
+        const percentageY = y/height - 0.5;
+        const rotX = percentageY * (pi/2 * factor);
+        const rotY = percentageX * (pi * factor);
+
+        return [ rotX, rotY, 0, 'YXZ' ] as [ number, number, number, 'YXZ' ]
+    }
+    useFrame(() => {
+        if (groupRef.current) {
+            groupRef.current.rotation.set(...getRotation());
+        }
+    })
+
     return (
-        <>
+        <group ref={groupRef}>
             { meshes.map(mesh => (
                 <mesh geometry={mesh.geometry}>
                     <meshStandardMaterial
@@ -42,7 +62,7 @@ const Model = () => {
                     />
                 </mesh>
             ))}
-        </>
+        </group>
     )
 }
 
--- src/contexts/ViewportContext/ViewportContext.tsx
+++ src/contexts/ViewportContext/ViewportContext.tsx
@@ -2,52 +2,62 @@ import React, { createContext } from 'react';
 import { useMediaQuery } from './useMediaQuery';
 
 const ViewportContext = createContext({
-  viewport: {
-    mobile: false,
-    tablet: false,
-    desktop: false,
-    type: ''
-  },
-  orientation: {
-    horizontal: false,
-    vertical: false
-  }
+	viewport: {
+		mobile: false,
+		tablet: false,
+		desktop: false,
+		type: ''
+	},
+	orientation: {
+		horizontal: false,
+		vertical: false
+	},
+	measurements: {
+		width: 0,
+		height: 0
+	}
 });
 
 const BREAKPOINTS = {
-  mobile: 200, //?
-  tablet: 768,
-  desktop: 1000 //1200
+	mobile: 200, //?
+	tablet: 768,
+	desktop: 1000 //1200
 };
 
 const ViewportProvider = ({ children }: { children: any }) => {
-  const min = (num: number) => `(min-width: ${num}px)`;
-  const desktop = useMediaQuery(min(BREAKPOINTS.desktop));
-  const isTablet = useMediaQuery(min(BREAKPOINTS.tablet));
-  const isMobile = useMediaQuery(min(BREAKPOINTS.mobile));
+	const min = (num: number) => `(min-width: ${num}px)`;
+	const desktop = useMediaQuery(min(BREAKPOINTS.desktop));
+	const isTablet = useMediaQuery(min(BREAKPOINTS.tablet));
+	const isMobile = useMediaQuery(min(BREAKPOINTS.mobile));
 
-  const tablet = isTablet && !desktop;
-  const mobile = isMobile && !tablet && !desktop;
+	const tablet = isTablet && !desktop;
+	const mobile = isMobile && !tablet && !desktop;
 
-  const type = desktop ? 'desktop' : tablet ? 'tablet' : mobile ? 'mobile' : '';
+	const type = desktop ? 'desktop' : tablet ? 'tablet' : mobile ? 'mobile' : '';
 
-  const horizontal = useMediaQuery('(orientation: landscape)');
-  const vertical = useMediaQuery('(orientation: portrait)');
+	const horizontal = useMediaQuery('(orientation: landscape)');
+	const vertical = useMediaQuery('(orientation: portrait)');
 
-  const props = {
-    viewport: { mobile, tablet, desktop, type },
-    orientation: { horizontal, vertical }
-  };
+	const measurements = {
+		width: window.innerWidth,
+		height: window.innerHeight
+	}
 
-  return (
-    <ViewportContext.Provider value={props}>
-      {children}
-    </ViewportContext.Provider>
-  );
+	const props = {
+		viewport: { mobile, tablet, desktop, type },
+		orientation: { horizontal, vertical },
+		measurements
+	};
+
+	return (
+		<ViewportContext.Provider value={props}>
+		{children}
+		</ViewportContext.Provider>
+	);
 };
 
 const useViewportContext = () => {
-  return React.useContext(ViewportContext);
+  	return React.useContext(ViewportContext);
 };
 
 export { ViewportProvider, useViewportContext };
