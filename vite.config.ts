47a830867f4c4d1a0cce80dcd4b9d96b3b11b01a
Polina <fokichevap@gmail.com>
2024-10-10 12:31:41 +0100

    Add vite legacy + dependency
    An attempt to fix white screen issue on older browsers. Not working yet, gonna try with sentry next (separate commit)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl(), svgr()],
+  plugins: [
+    react(),
+    basicSsl(),
+    svgr(),
+    legacy({
+      targets: ['chrome >= 64', 'safari >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
 })
