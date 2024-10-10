d921f06256b0a41d32ddbc932ab1807397587c91
Polina <fokichevap@gmail.com>
2024-10-10 23:54:19 +0100

    add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,26 @@ import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import svgr from "vite-plugin-svgr";
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl(), svgr(), legacy({
-    targets: ['chrome >= 64', 'safari >= 12'],
-    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-  }), sentryVitePlugin({
-    org: "lev-fokichev",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    basicSsl(),
+    svgr(),
+    legacy({
+      targets: ['chrome >= 64', 'safari >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    }),
+    sentryVitePlugin({
+      org: "lev-fokichev",
+      project: "javascript-react"
+    }),
+    VitePluginRadar({
+      analytics: { id: 'G-PDTPDGR4E1' }
+    })
+  ],
   build: {
     sourcemap: true
   }
