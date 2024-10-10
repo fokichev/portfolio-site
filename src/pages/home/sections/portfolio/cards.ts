10f37eeed5d51c703166efdca965d1f0287c9082
Polina <fokichevap@gmail.com>
2024-10-10 12:02:29 +0100

    Add more recent portfolio pieces, clean up scroll-bar scss

--- src/pages/home/sections/portfolio/cards.ts
+++ src/pages/home/sections/portfolio/cards.ts
@@ -1,11 +1,18 @@
+import KulinaryImg from '../../../../assets/portfolio/kulinary.png';
 import PersonalWebsiteImg from '../../../../assets/portfolio/personal-website.png';
+import PersonalWebsiteOldImg from '../../../../assets/portfolio/personal-website-old.png';
 import MailchimpServerImg from '../../../../assets/portfolio/mailchimp-server.png';
-import HoldingPageImg from '../../../../assets/portfolio/holding-page.png';
-import ArtistPortfolioImg from '../../../../assets/portfolio/artist-portfolio.png';
 import PixelIslandImg from '../../../../assets/portfolio/pixel-island.png';
 import TwitterWordCloudImg from '../../../../assets/portfolio/twitter-word-cloud.png';
 
 const CARDS = [
+    {
+        key: 'kulinary',
+        title: 'Kulinary (Chrome Extension)',
+        skills: ['TypeScript', 'NodeJS', 'ExpressJS', 'Zod', 'Chrome Extensions API', 'ChatGPT API', 'Spoontacular API'],
+        image: KulinaryImg,
+        link: 'https://chromewebstore.google.com/detail/kulinary/mpjdadlofjgdhlaocbdjohlmmffekpan'
+    },
     {
         key: 'personal-website',
         title: 'Personal Website',
@@ -14,26 +21,33 @@ const CARDS = [
         link: 'https://github.com/fokichev/portfolio-site'
     },
     {
+        key: 'personal-website-old',
+        title: 'Personal Website (Old ver)',
+        skills: ['React', 'TypeScript'],
+        image: PersonalWebsiteOldImg,
+        link: 'https://github.com/fokichev/personal-portfolio'
+    },
+    {
         key: 'mailchimp-server',
         title: 'Mailchimp API Server',
-        skills: ['NodeJS', 'JavaScript'],
+        skills: ['NodeJS', 'ExpressJS', 'JavaScript'],
         image: MailchimpServerImg,
         link: 'https://holdingpage1.netlify.app/'
     },
-    {
-        key: 'holding-page',
-        title: 'Holding Page',
-        skills: ['React', 'TypeScript'],
-        image: HoldingPageImg,
-        link: 'https://holdingpage2.netlify.app/'
-    },
-    {
-        key: 'artist-portfolio',
-        title: 'Artist Portfolio',
-        skills: ['React', 'TypeScript', 'Sass'],
-        image: ArtistPortfolioImg,
-        link: 'https://klarafokicheva.com/'
-    },
+    // {
+    //     key: 'holding-page',
+    //     title: 'Holding Page',
+    //     skills: ['React', 'TypeScript'],
+    //     image: HoldingPageImg,
+    //     link: 'https://holdingpage2.netlify.app/'
+    // },
+    // {
+    //     key: 'artist-portfolio',
+    //     title: 'Artist Portfolio',
+    //     skills: ['React', 'TypeScript', 'Sass'],
+    //     image: ArtistPortfolioImg,
+    //     link: 'https://klarafokicheva.com/'
+    // },
     {
         key: 'pixel-island',
         title: 'Pixel Island',
