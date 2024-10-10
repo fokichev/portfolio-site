cc9f7cb5b2190b86a7b433ccc1157e8c38c5a20b
Polina <fokichevap@gmail.com>
2024-10-10 13:48:12 +0100

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import App from './App.tsx'
 import './index.scss'
+
+Sentry.init({
+	dsn: "https://42b8fc64e35921ff274a457fcdb0a0b4@o4508098216525824.ingest.de.sentry.io/4508098223013968",
+	integrations: [
+	  Sentry.browserTracingIntegration(),
+	  Sentry.replayIntegration(),
+	],
+	// Tracing
+	// tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+	// While front end only, don't need this:
+	// tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
@@ -6,13 +7,14 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl(),
-    svgr(),
-    legacy({
-      targets: ['chrome >= 64', 'safari >= 12'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [react(), basicSsl(), svgr(), legacy({
+    targets: ['chrome >= 64', 'safari >= 12'],
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  }), sentryVitePlugin({
+    org: "lev-fokichev",
+    project: "javascript-react"
+  })],
+  build: {
+    sourcemap: true
+  }
 })
